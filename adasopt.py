78ad3206478a83761a6d56f4ea1f2960bb32ad56
Yanai <none@none.com>
2021-01-14 22:46:07 +0200

    made adasopt.py compatible with both tensorflow v1 and v2

--- adasopt.py
+++ adasopt.py
@@ -25,6 +25,11 @@
 from tensorflow.keras.optimizers import Optimizer
 from tensorflow.keras import backend as K
 from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import state_ops
+from tensorflow.python.training import moving_averages
+
+import tensorflow as tf
 
 class AdasOptimizer(Optimizer):
     """Adas optimizer.
@@ -42,54 +47,104 @@ class AdasOptimizer(Optimizer):
             beta_1 = 0.999, beta_2 = 0.999, beta_3 = 0.9999,
             epsilon = None, **kwargs):
         super(AdasOptimizer, self).__init__('Adas',**kwargs)
-        self._iterations = K.variable(0, dtype='int64', name='iterations')
         self._lr = lr
         self._lr2 = lr2
         self._lr3 = lr3
-        self._beta_1 = K.variable(beta_1, name='beta_1')
-        self._beta_2 = K.variable(beta_2, name='beta_2')
-        self._beta_3 = K.variable(beta_3, name='beta_3')
+        self._beta_1 = beta_1
+        self._beta_2 = beta_2
+        self._beta_3 = beta_3
         if epsilon is None:
             epsilon = K.epsilon()
         self._epsilon = epsilon
+        self._tf1 = tf.__version__.split(".")[0] == '1'
+        if not self._tf1:
+            self._varn = None
+            self._is_create_slots = None
+            self._curr_var = None
 
-    def _derivatives_normalizer(self,derivatives,beta):
-        t = K.cast(self._iterations, K.floatx()) + 1
+    def _assign(self,x,y):
+        if self._tf1:
+            result = K.update(x,y)
+        else:
+            result = state_ops.assign(x,y,use_locking=self._use_locking)
+        self._updates.append(result)
+        return result
+
+    def _add(self,x,y):
+        if self._tf1:
+            result = K.update_add(x,y)
+        else:
+            result = state_ops.assign_add(x,y,use_locking=self._use_locking)
+        self._updates.append(result)
+        return result
+
+    def _moving_average(self,var,value,momentum):
+        if self._tf1:
+            return self._assign(var,var * momentum + value * (1 - momentum))
+        result = K.moving_average_update(var,value,momentum)
+        self._updates.append(result)
+        return result
+
+    def _derivatives_normalizer(self,derivative,beta):
+        if self._tf1:
+            self._iterations = self._make_variable()
+            self._add(self._iterations,1)
+        t = K.cast(self._iterations if self._tf1 else self.iterations, K.floatx()) + 1
         lr_t = K.sqrt(1. - K.pow(self._beta_1, t))
-        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in derivatives]
-        new_derivatives = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in derivatives]
-        new_derivatives2 = []
-        old_moments = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in derivatives]
-        new_moments = []
-        for p, p_t, m, om in zip(derivatives, new_derivatives, ms, old_moments):
-            v_t = (self._beta_1 * m) + (1. - self._beta_1) * K.square(p)
-            self._updates.append(K.update(m, v_t))
-            np_t = p * lr_t / (K.sqrt(v_t) + self._epsilon)
-            m_t = (beta * om) + (1. - beta) * np_t
-            self._updates.append(K.update(om, m_t))
-            new_moments.append(m_t)
-            new_derivatives2.append(np_t)
-            self._updates.append(K.update(p_t,np_t))
-        return (old_moments,new_derivatives2)
+        m = self._make_variable(0,K.int_shape(derivative),K.dtype(derivative))
+        old_moments = self._make_variable(0,K.int_shape(derivative),K.dtype(derivative))
+        old_moments2 = self._make_variable(0,K.int_shape(derivative),K.dtype(derivative))
+        res = self._assign(old_moments2,old_moments)
+        v_t = self._moving_average(m,K.square(derivative),self._beta_1)
+        with tf.control_dependencies([v_t]):
+            np_t = derivative * lr_t / (K.sqrt(v_t) + self._epsilon)
+        with tf.control_dependencies([np_t]):
+            m_t = self._moving_average(old_moments,np_t,beta)
+        return (res,np_t)
+
+    def _make_variable(self,value = 0,shape = (),dtype = K.floatx()):
+        if self._tf1:
+            return K.variable(K.constant(value,shape=shape, dtype=dtype))
+        self._varn += 1
+        name = 'unnamed_variable' + str(self._varn)
+        if self._is_create_slots:
+            return self.add_slot(self._curr_var,name,initializer = K.constant(value,shape=shape, dtype=dtype))
+        else:
+            return self.get_slot(self._curr_var,name)
+
+    def _get_updates_universal_impl(self, grad, param):
+        self._updates = []
+        lr = self._make_variable(value = self._lr,shape=K.int_shape(param)[:-1], dtype=K.dtype(param))
+        moment, deriv = self._derivatives_normalizer(grad,self._beta_3)
+        param_t = self._add(param, - K.expand_dims(lr,len(K.int_shape(param)) - 1) * deriv)
+        with tf.control_dependencies([moment]):
+            lr_deriv = math_ops.reduce_sum(moment * grad,len(K.int_shape(param)) - 1)
+        master_lr = self._make_variable(self._lr2)
+        m2,d2 = self._derivatives_normalizer(lr_deriv,self._beta_2)
+        lr_t = self._add(lr,master_lr * lr * d2)
+        with tf.control_dependencies([m2]):
+            master_lr_deriv2 = math_ops.reduce_sum(m2 * lr_deriv)
+        m3,d3 = self._derivatives_normalizer(master_lr_deriv2,0.)
+        with tf.control_dependencies([m3]):
+            self._add(master_lr,self._lr3 * master_lr * d3)
+        return self._updates
+
+    def _get_updates_universal(self, param, grad = None, is_create_slots = False):
+        self._curr_var = param
+        self._is_create_slots = is_create_slots
+        self._varn = 0
+        return self._get_updates_universal_impl(grad if grad is not None else K.constant(0,shape=param.shape,dtype=K.dtype(param)),param)
 
     def get_updates(self, loss, params):
         grads = [a for a in self.get_gradients(loss, params)]
-        self._updates = [K.update_add(self._iterations, 1)]
-        lrs = [K.variable(K.constant(self._lr,shape=K.int_shape(p)[:-1], dtype=K.dtype(p))) for p in params]
-        moments, derivs = self._derivatives_normalizer(grads,self._beta_3)
-        for lr,deriv,moment,param,deriv2 in zip(lrs,derivs,moments,params,grads):
-            param_t = param - K.expand_dims(lr,len(K.int_shape(param)) - 1) * deriv
-            self._updates.append(K.update(param, param_t))
-            lr_deriv = math_ops.reduce_sum(moment * deriv2,len(K.int_shape(param)) - 1)
-            master_lr = K.variable(self._lr2)
-            m2,d2 = self._derivatives_normalizer([lr_deriv],self._beta_2)
-            lr_t = lr + master_lr * lr * d2[0]
-            self._updates.append(K.update(lr, lr_t))
-            master_lr_deriv2 = math_ops.reduce_sum(m2[0] * lr_deriv)
-            m3,d3 = self._derivatives_normalizer([master_lr_deriv2],0.)
-            master_lr_t = master_lr + self._lr3 * master_lr * d3[0]
-            self._updates.append(K.update(master_lr, master_lr_t))
-        return self._updates
+        return sum([self._get_updates_universal_impl(grad,var) for (grad,var) in zip(grads,params)],[])
+
+    def _create_slots(self, var_list):
+        for var in var_list:
+            self._get_updates_universal(var,is_create_slots = True)
+
+    def _resource_apply_dense(self, grad, var):
+        return control_flow_ops.group(*self._get_updates_universal(var,grad))
 
     def get_config(self):
         config = {
